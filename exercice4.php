b8638dae90330b6e5a9339798874de4e267387d3
Stanis Humez <stanis@netvolution.fr>
2020-03-18 15:19:20 +0100

    Ajout de l'exercice 4

---
+++ exercice4.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulaire PHP</title>
+</head>
+<body>
+<form action="exercice4-traitement.php">
+    <input type="text" name="nom" placeholder="Votre nom">
+    <input type="text" name="prenom" placeholder="Votre prénom">
+    <button>Valider le formulaire</button>
+</form>
+</body>
+</html>
